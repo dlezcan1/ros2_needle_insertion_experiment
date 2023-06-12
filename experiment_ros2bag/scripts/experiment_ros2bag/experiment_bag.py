86a6dd015635ed8605fce67f6cb50e2f1beaf7d5
Dimitri Lezcano <dlezcan1@jhu.edu>
2023-06-12 12:19:39 -0400

    Updated experiment bag processor

--- experiment_ros2bag/scripts/experiment_ros2bag/experiment_bag.py
+++ experiment_ros2bag/scripts/experiment_ros2bag/experiment_bag.py
@@ -4,6 +4,11 @@ from .bag_file_parser import BagFileParser
 import pandas as pd
 import numpy as np
 
+from needle_shape_publisher.utilites import (
+    msg2pose,
+    msg2poses,
+)
+
 
 class InsertionExperimentBag( BagFileParser ):
     DEFAULT_TOPICS_OF_INTEREST = {
@@ -29,9 +34,9 @@ class InsertionExperimentBag( BagFileParser ):
         self.topics_of_interest = topics if topics is not None else InsertionExperimentBag.DEFAULT_TOPICS_OF_INTEREST
 
         # data containers
-        self.camera_data = []
-        self.robot_data  = []
-        self.needle_data = []
+        self.camera_data = list()
+        self.robot_data  = list()
+        self.needle_data = list()
 
     # __init__
 
@@ -54,29 +59,57 @@ class InsertionExperimentBag( BagFileParser ):
                 # parse the set of rows
                 try:
                     robot_positions = {topic.replace('/stage/axis/position/', '') : (ts, msg.data) for ts, topic, msg in rows}
-                # timestamp = min([ts for ts, *_ in robot_positions.values()])
                 
-
                     # make sure this set has all 4 messages
                     if len(robot_positions) != len(robot_position_topics):
                         continue
                     print(  robot_positions['x'][0]     )
                     # append to robot data
-                    tupl=( robot_positions['x'][0], 
-                          robot_positions['x'][1],
-                          robot_positions['y'][1],
-                          robot_positions['z'][1],
-                          robot_positions['linear_stage'][1] )
+                    tupl=( 
+                        robot_positions['x'][0], 
+                        robot_positions['x'][1],
+                        robot_positions['y'][1],
+                        robot_positions['z'][1],
+                        robot_positions['linear_stage'][1]
+                    )
+                    robot_state = {
+                        'x': {
+                            'timestamp': robot_positions['x'][0], 
+                            'position': robot_positions['x'][1],
+                        },
+                        'y': {
+                            'timestamp': robot_positions['y'][0], 
+                            'position': robot_positions['y'][1],
+                        },
+                        'z': {
+                            'timestamp': robot_positions['z'][0], 
+                            'position': robot_positions['z'][1],
+                        },
+                        'linear_stage': {
+                            'timestamp': robot_positions['linear_stage'][0], 
+                            'position': robot_positions['linear_stage'][1],
+                        },
+                    }
                     self.robot_data.append(
                         tupl
                     )
                     print(tupl)
                 except:
-                    print("exception caught")
-                    df = pd.DataFrame(np.array(self.robot_data))
+                    print("[ROBOT DATA PROCESSING]: exception caught")
+                finally:
+                    df = pd.DataFrame(
+                        np.array(self.robot_data),
+                        columns=[
+                            'timestamp x', 'position x',
+                            'timestamp y', 'position y',
+                            'timestamp z', 'position z',
+                            'timestamp linear_stage', 'position linear_stage',
+                        ]    
+                    )
                     df.to_csv('axes_positions4.csv', index=True)
                     print("saved axes positions")
                     break
+                
                 # debugging stuff
                 # print( f"[{timestamp}]:" )
                 # for axis, val in sorted(robot_positions.items()):
@@ -90,7 +123,7 @@ class InsertionExperimentBag( BagFileParser ):
         # if: robot
 
         # parse needle data
-        if needle: # TODO
+        if needle: 
             # get the needle messages as a generator (to not overload RAM)
             needle_shape_topics = list(filter(lambda t: "/needle/state/current_shape" in t, self.topics_of_interest['needle']))
             bag_rows = self.get_messages(topic_name=needle_shape_topics, generator_count=len(needle_shape_topics))
@@ -98,43 +131,44 @@ class InsertionExperimentBag( BagFileParser ):
             # iterate through the generator
             for i, rows in enumerate(bag_rows):
                 # parse the set of rows
-                
-                robot_positions = {
+                needle_positions = {
                     topic.replace('/needle/state/current_shape/', '') : (ts, msg.poses) for ts, topic, msg in rows
                 }
-                # timestamp = min([ts for ts, *_ in robot_positions.values()])
-
-                # print( timestamp, robot_positions["/needle/state/current_shape"][1][0].position    )
+                
                 try:        
-                    shape=[]
-                    for p in robot_positions["/needle/state/current_shape"][1]:
-                        shape.append([p.position.x, p.position.y, p.position.z])
+                    shape = msg2poses(needle_positions["/needle/state/current_shape"][1])[:, :3, 3]
 
-                    
-
-                    # make sure this set has all 4 messages
-                    if len(robot_positions) != len(needle_shape_topics):
+                    # make sure this set has all required messages
+                    if len(needle_positions) != len(needle_shape_topics):
                         continue
-                    print(robot_positions["/needle/state/current_shape"][0])
-                    #append to robot data
+                    
+                    print(needle_positions["/needle/state/current_shape"][0])
+                
+                    # append to needle data
                     self.needle_data.append(
-                        ( robot_positions["/needle/state/current_shape"][0], 
+                        ( needle_positions["/needle/state/current_shape"][0], 
                           shape)
                     )
+
                 except:
+                    print(f"[NEEDLE DATA PROCESSING]: There was an error at timestamp: {needle_positions['/needle/state/current_shape'][0]}")
+
+                finally:
                     df = pd.DataFrame(np.array(self.needle_data))
                     df.to_csv('needle_shapes4.csv', index=True)
                     print("saved needle shapes")
                     break
-            print("end of parsing")
-                # # debugging stuff
-                # print( f"[{timestamp}]:" )
-                # for axis, val in sorted(robot_positions.items()):
-                #     print(f"  {axis:15s}: {val[1]:10.6f} mm")
-                # print(100*"=")
 
-                # if i >= 6:
-                #     break
+            print("end of parsing")
+            
+            # # debugging stuff
+            # print( f"[{timestamp}]:" )
+            # for axis, val in sorted(robot_positions.items()):
+            #     print(f"  {axis:15s}: {val[1]:10.6f} mm")
+            # print(100*"=")
+
+            # if i >= 6:
+            #     break
             
 
 
